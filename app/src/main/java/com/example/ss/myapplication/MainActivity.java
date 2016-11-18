350b4d9f852b6d4a1564d9678d288b3326306020
최성근 <ccc3132@naver.com>
2016-11-18 18:49:52 +0900

    수업자료 완성

--- app/src/main/java/com/example/ss/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ss/myapplication/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.animation.Interpolator;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText t1;
@@ -16,23 +17,29 @@ public class MainActivity extends AppCompatActivity {
     Button button2;
     Button button3;
     Button button4;
+    Button button5;
 
     TextView tv;
-    int result;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        init();
+    }
+
+    void init(){
         t1 = (EditText) findViewById(R.id.edit1);
         t2 = (EditText) findViewById(R.id.edit2);
-
         tv = (TextView) findViewById(R.id.textView2);
 
         button1 = (Button) findViewById(R.id.button);
         button2 = (Button) findViewById(R.id.button2);
         button3 = (Button) findViewById(R.id.button3);
         button4 = (Button) findViewById(R.id.button4);
+        button5 = (Button) findViewById(R.id.button5);
+
 
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,24 +66,47 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+        button5.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                calc(5);
+            }
+        });
     }
-    private void calc(int a){
-        switch (a){
+    private void calc(int a) {
+        if (t1.getText().length() == 0 || t2.getText().length() == 0) {
+            Toast.makeText(getApplicationContext(), " 값을 넣지 않으였습니다..", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
+        double result = 0;
+        double num1 = Double.parseDouble(t1.getText().toString());
+        double num2 = Double.parseDouble(t2.getText().toString());
+
+
+        switch (a) {
             case 1:
-                result = Integer.parseInt(t1.getText().toString())+Integer.parseInt(t2.getText().toString());
+                result = num1 + num2;
                 break;
             case 2:
-                result = Integer.parseInt(t1.getText().toString())-Integer.parseInt(t2.getText().toString());
+                result = num1 - num2;
                 break;
             case 3:
-                result = Integer.parseInt(t1.getText().toString())*Integer.parseInt(t2.getText().toString());
+                result = num1 * num2;
                 break;
             case 4:
-                result = Integer.parseInt(t1.getText().toString())/Integer.parseInt(t2.getText().toString());
+                if(num1 == 0 || num2 == 0)
+                {
+                    Toast.makeText(getApplicationContext(), " 0으로 나눴습니다.", Toast.LENGTH_SHORT).show();
+                }
+                result = num1 / num2;
+
+                break;
+            case 5:
+                result = num1 % num2;
                 break;
         }
-        tv.setText("계산결과:"+result);
+        tv.setText("계산결과:" + result);
     }
-
 }
 
